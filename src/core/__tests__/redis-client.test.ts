c50b977037898aa678fdc7a1004c25fdf0a8dad0
fulviix <fulviodipietro08@icloud.com>
2026-09-08 23:04:26 +0200

    test: tested if redis client can write and read a key

---
+++ src/core/__tests__/redis-client.test.ts
@@ -0,0 +1,17 @@
+import { describe, it, afterEach, expect } from "vitest";
+import {Redis} from "ioredis"
+import { createRedisClient } from "../redis-client.js";
+describe("createRedisClient", ()=>{
+    let client: Redis | undefined
+    afterEach(async ()=>{
+        await client?.quit()
+        client = undefined
+    })
+    it('it can write and read a test key', async ()=>{
+        client = createRedisClient({host: '127.0.0.1', port: 6379})
+        await client.set('test:connection', 'ok')
+        const value = await client.get('test:connection')
+        expect(value).toBe('ok')
+        await client.del('test:connection')
+    })
+})
